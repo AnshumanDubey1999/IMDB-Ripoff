57d9369c96dc5e103874a39e7963bb37b5d1fca8
Anshuman Dubey <anshumandubey2k18@gmail.com>
2020-04-02 19:15:25 +0530

    Added dotenv package

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs")
+require("dotenv").config()
 
 
 app.get("/results", function(req, res){
